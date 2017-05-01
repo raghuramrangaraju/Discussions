c980c6549bc5f6d8bd8951db43b8c1cee4576882
rahuram777 <rahuram777@gmail.com>
2017-05-01 10:22:16 -0400

    New logic for AJAX

--- Discussions/Controllers/HomeController.cs
+++ Discussions/Controllers/HomeController.cs
@@ -29,26 +29,53 @@ namespace Discussions.Controllers
         {
             ViewBag.questionid = QuestionId;
             ViewBag.question = context.Questions.Where(x => x.QuestionId == QuestionId).Select(x => x.Question1).FirstOrDefault();
-            ViewBag.yestance = context.Claims.Where(x => x.QuestionId == QuestionId && x.Status == true).OrderByDescending(x=>x.Rating).ToList();
+            var stance = (from a in context.Questions
+                          join b in context.StanceTypes on a.QuestionId equals b.QuestionId
+                          where a.QuestionId == QuestionId 
+                            select new 
+                            {
+                                b.StanceTypeName,
+                                b.StanceTypeId,
+                                b.QuestionId
+                            }).ToList();
+
+            // -----------------  To find the recorded Claim for the question -------
+
+            List<claimstance> mainclaimstance = new List<claimstance>();
+
+            foreach (var item in stance) {
+                claimstance claimstance = new claimstance();
+                claimstance.StanceTypeName = item.StanceTypeName;
+                claimstance.StanceTypeId = item.StanceTypeId;
+                claimstance.QuestionId = item.QuestionId;
+                claimstance.Claim = context.Claims.Where(x => x.QuestionId == item.QuestionId && x.StanceTypeId == item.StanceTypeId).ToList();
+                mainclaimstance.Add(claimstance);
+            }
+            ViewBag.dynamicstance = mainclaimstance;
+
+            //var stanceExample = context.ClaimStanceFunct(QuestionId).ToList();
+
+            ViewBag.yestance = context.Claims.Where(x => x.QuestionId == QuestionId && x.Status == true).OrderByDescending(x => x.Rating).ToList();
             ViewBag.notance = context.Claims.Where(x => x.QuestionId == QuestionId && x.Status == false).OrderByDescending(x => x.Rating).ToList();
-          //  var yes = context.Claims.Where(x => x.QuestionId == QuestionId && x.Status == true).ToList();
-          //  ViewBag.yestance = yes.OrderBy(x => x.Rating);
-          //var no = context.Claims.Where(x => x.QuestionId == QuestionId && x.Status == false).ToList();
-          //  ViewBag.notance = no;
+            //  var yes = context.Claims.Where(x => x.QuestionId == QuestionId && x.Status == true).ToList();
+            //  ViewBag.yestance = yes.OrderBy(x => x.Rating);
+            //var no = context.Claims.Where(x => x.QuestionId == QuestionId && x.Status == false).ToList();
+            //  ViewBag.notance = no;
             return View();
         }
         [HttpPost]
-        public ActionResult Claim(Claim claim)
+        public ActionResult Claim(Claim claim, FormCollection value)
         {
             Claim newclaim = new Claim();
             newclaim.Claim1 = claim.Claim1;
             newclaim.CreatedAt = DateTime.UtcNow;
             newclaim.Evidence = claim.Evidence;
             newclaim.Source = claim.Source;
-            newclaim.Status = claim.Status; // For no stance and yes for new stance
+            newclaim.StanceTypeId = Convert.ToInt64(claim.StanceTypeId); // For no stance and yes for new stance
             newclaim.UserId = Convert.ToInt64(Session["UserId"]);  // Its has to be changed 
             newclaim.QuestionId = claim.QuestionId;
             newclaim.Rating = 0; // Initial rating will be zero
+            newclaim.Stance = value["claimcheckbox"];
             context.Claims.Add(newclaim);
             context.SaveChanges();
             return RedirectToAction("Claim", new { QuestionId = newclaim.QuestionId });
@@ -62,28 +89,43 @@ namespace Discussions.Controllers
         }
 
 
-        public JsonResult askquestion(string question = "")
+        public JsonResult askquestion(string question = "", string Stance = "")
         {
+
+
+
             //To check the user first and get the username 
             var newdata = new Question();
             newdata.Question1 = question;
             newdata.UserId = Convert.ToInt64(Session["UserId"]);
             newdata.CreateAt = DateTime.UtcNow;
             newdata.Status = true;
-            //dbCtx.Entry(newStudent).State = System.Data.Entity.EntityState.Added;
-
-            //call SaveChanges method to save new Student into database
-            //dbCtx.SaveChanges();
             context.Entry(newdata).State = EntityState.Added;
             context.SaveChanges();
-            return Json(newdata, JsonRequestBehavior.AllowGet);
+
+            // Parse the string 
+            string[] stringbuffer = Stance.Split('$');
+            foreach (var item in stringbuffer)
+            {
+                if (item != "" && item != " ")
+                {
+                    var newStanceType = new StanceType();
+                    newStanceType.CreatedAt = DateTime.UtcNow;
+                    newStanceType.QuestionId = newdata.QuestionId;
+                    newStanceType.StanceTypeName = item;
+                    context.Entry(newStanceType).State = EntityState.Added;
+                    context.SaveChanges();
+                }
+            }
+            return Json(newdata.QuestionId, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult vote(int? questionid, int? claimid, bool? status, bool? stance)
         {
             var userid = Convert.ToInt64(Session["UserId"]);
             var data = context.Votes.Where(x => x.QuestionId == questionid && x.ClaimId == claimid && x.UserId == userid && x.Stance == stance && x.UserId == userid).FirstOrDefault();
-            if (data == null){ // new value
+            if (data == null)
+            { // new value
                 Vote newdata = new Vote();
                 newdata.ClaimId = claimid;
                 newdata.CreatedAt = DateTime.UtcNow;
